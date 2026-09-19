eb2fb1b8cc9181c387e7d0a2ac441499a0a092e9
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-09-19 19:19:30 +0530

    chore: add production Dockerfile and CI/CD workflow

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+        ...globals.jest,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-undef': 'warn',
+      'no-control-regex': 'off',
+    },
+  },
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/coverage/**',
+      '**/build/**',
+      '**/.eslintrc.js',
+    ],
+  },
+];
